c6578a26d0dd675a6027c1ab2763408659e03fb6
Fabio Luis <ml@fabiotecnico1.biz>
2022-01-11 20:24:19 -0300

    Finished component Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,8 @@
-import {ReactComponent as GithubIcon} from './assets/img/github.svg';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <header>
-      <nav>
-        <div>
-          <h1>DSMovie</h1>
-          <a href="https://github.com/Flgc">
-            <div>
-              <GithubIcon />
-              <p>/Flgc</p>
-            </div>
-          </a>
-        </div>
-      </nav>
-    </header>
+    <Navbar />
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css'
+function Navbar(){
+    return (
+        <header>
+        <nav className="container">
+          <div className="dsmovie-navbar-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/Flgc">
+              <div className="dsmovie-contact-container">
+                <GithubIcon />
+                <p className="dsmovie-contact-link">/Flgc</p>
+              </div>
+            </a>
+          </div>
+        </nav>
+      </header>
+      );
+}
+
+export default Navbar;
