77489f2768966b7660bebe0eae5787b34b995419
Fabio Luis <ml@fabiotecnico1.biz>
2022-01-11 19:12:22 -0300

    Component Navbar created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,20 @@
+import {ReactComponent as GithubIcon} from './assets/img/github.svg';
+
 function App() {
   return (
-    <h1>Página principal</h1>
+    <header>
+      <nav>
+        <div>
+          <h1>DSMovie</h1>
+          <a href="https://github.com/Flgc">
+            <div>
+              <GithubIcon />
+              <p>/Flgc</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
   );
 }
 
