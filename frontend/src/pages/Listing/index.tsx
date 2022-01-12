a01b427d408879f719ed9d2f4f28169105d851c0
Fabio Luis <ml@fabiotecnico1.biz>
2022-01-12 19:34:37 -0300

    MovieCard Finished

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,4 +1,4 @@
-import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
@@ -6,7 +6,26 @@ function Listing() {
     return (
         <>
             <Pagination />
-            <MovieScore />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div>                                                                              
+                </div>   
+            </div>
         </>
     )
 }
